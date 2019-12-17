77324f961f495732d20a4454c516fb0f1eb332a4
adrian61 <ad.kowalski@gmail.com>
2019-12-17 11:16:12 +0100

    Stałe i adnotacje

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,33 +2,33 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-	private int[] NUMBERS = new int[12];
+    private static final int NONE = -1;
+    private static final int SIZE = 12;
+    private int[] NUMBERS = new int[SIZE];
 
-	public int total = -1;
+    public int total = NONE;
 
-	public void countIn(int in) {
-		if (!isFull())
-			NUMBERS[++total] = in;
-	}
+    public void countIn(int in) {
+        if (!isFull())
+            NUMBERS[++total] = in;
+    }
 
-	public boolean callCheck() {
-		return total == -1;
-	}
-		
-	public boolean isFull() {
-		return total == 11;
-	}
-		
-	protected int peekaboo() {
-		if (callCheck())
-			return -1;
-		return NUMBERS[total];
-	}
-			
-	public int countOut() {
-		if (callCheck())
-			return -1;
-		return NUMBERS[total--];
-	}
+    public boolean callCheck() {
+        return total == NONE;
+    }
+
+    public boolean isFull() {
+        return total == 11;
+    }
+    protected int peekaboo() {
+        if (callCheck())
+            return NONE;
+        return NUMBERS[total];
+    }
+    public int countOut() {
+        if (callCheck())
+            return NONE;
+        return NUMBERS[total--];
+    }
 
 }
