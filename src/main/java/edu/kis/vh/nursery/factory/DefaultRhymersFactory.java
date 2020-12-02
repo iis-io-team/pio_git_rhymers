c8213b248f4103dd3d388adbbe3164c9ec8a0736
t-ogrodowski <t.ogrodowski99@gmail.com>
2020-12-02 12:18:40 +0100

    1.3 poprawienie formatowania kodu

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -5,26 +5,31 @@ import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
-public class DefaultRhymersFactory implements Rhymersfactory {
+public class DefaultRhymersFactory implements Rhymersfactory
+{
 
-	@Override
-	public defaultCountingOutRhymer GetStandardRhymer() {
-		return new defaultCountingOutRhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetStandardRhymer()
+    {
+        return new defaultCountingOutRhymer();
+    }
 
-	@Override
-	public defaultCountingOutRhymer GetFalseRhymer() {
-		return new defaultCountingOutRhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetFalseRhymer()
+    {
+        return new defaultCountingOutRhymer();
+    }
 
-	@Override
-	public defaultCountingOutRhymer GetFIFORhymer() {
-		return new FIFORhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetFIFORhymer()
+    {
+        return new FIFORhymer();
+    }
 
-	@Override
-	public defaultCountingOutRhymer GetHanoiRhymer() {
-		return new HanoiRhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetHanoiRhymer()
+    {
+        return new HanoiRhymer();
+    }
 
 }
