189e8512ff05772230dd456458160a6d0ffe5ec9
t-ogrodowski <t.ogrodowski99@gmail.com>
2020-12-16 23:59:47 +0100

    1.3 poprawienie nazw pól klas

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,24 @@
 package edu.kis.vh.nursery;
 
-public class HanoiRhymer extends defaultCountingOutRhymer {
+public class HanoiRhymer extends DefaultCountingOutRhymer
+{
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected()
+    {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in)
+    {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
+
+//	zmodyfikowano linie 11, 12, 14 i 15
+//
+//	klawisze strzałek w połączeniu z klawiszem alt przełączają widok pomiędzy otwartymi plikami
 }
