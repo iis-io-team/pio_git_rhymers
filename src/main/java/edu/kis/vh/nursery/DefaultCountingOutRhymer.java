b1a50b5f9310d70c2d236ed365db6c9bc4defad2
Jan Oleksik <222529@edu.p.lodz.pl>
2021-01-19 17:55:52 +0100

    Zmiany w plikach: IntLinkedList, DefaultCountingOutRhymer, HanoiRhymer. Zmiana literalow na stale, dodanie modifikatora final oraz annotacji @Override

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    private final int NUMBER_ARRAY_SIZE=12;
+    public final int INDEX_ERROR=-1;
+    private int[] numbers = new int[NUMBER_ARRAY_SIZE];
 
-    public int index = -1;
+    public int index = INDEX_ERROR;
 
     public void countIn(int in) {
         if (!isFull())
@@ -13,22 +15,22 @@ public class DefaultCountingOutRhymer {
 
 
     public boolean isEmpty() {
-        return index == -1;
+        return index == INDEX_ERROR;
     }
 
     public boolean isFull() {
-        return index == 11;
+        return index == NUMBER_ARRAY_SIZE+INDEX_ERROR;
     }
 
     protected int peekaboo() {
         if (isEmpty())
-            return -1;
+            return INDEX_ERROR;
         return numbers[index];
     }
 
     public int countOut() {
         if (isEmpty())
-            return -1;
+            return INDEX_ERROR;
         return numbers[index--];
     }
 
