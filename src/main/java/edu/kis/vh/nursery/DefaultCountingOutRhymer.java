b814f3c5774cc9ecff969d6edd21c69383d9295b
240827 <240827@edu.p.lodz.pl>
2023-05-23 23:42:35 +0200

    dodanie dokumentacji do klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,9 @@
 package edu.kis.vh.nursery;
 
+/**
+ * @author 240827
+ *	Default rhymer that works similarly to a stack.
+ */
 public class DefaultCountingOutRhymer {
 
 	public static final int NUMBER = 12;
@@ -8,29 +12,53 @@ public class DefaultCountingOutRhymer {
 
 	private int total = NEGATIVE;
 
+	/**
+	 * @return How many numbers have been added to the rhymer so far.
+	 */
 	public int getTotal() {
 		return total;
 	}
 
+	/** 
+	 * Adds number specified in in argument to the rhymer.
+	 * The in argument is not added if the rhymer is full.
+	 * @param in
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+	/**
+	 * @return boolean value indicating if the rhymer is empty.
+	 */
 	public boolean isEmpty() {
 		return total == NEGATIVE;
 	}
 
+	/**
+	 * @return boolean value indicating if the rhymer is full.
+	 */
 	public boolean isFull() {
 		return total == 11;
 	}
 
-	protected int peekaboo() {
+	/**
+	 * Returns at the top of the rhymer.
+	 * If the rhymer is empty, NEGATIVE is returned.
+	 * @return value at the top of the rhymer.
+	 */
+	protected int peekaboo() { 
 		if (isEmpty())
 			return NEGATIVE;
 		return numbers[total];
 	}
 
+	/**
+	 * Returns value at the top of the rhymer and removes it from the rhymer.
+	 * If the rhymer is empty, NEGATIVE is returned.
+	 * @return value at the top of the rhymer.
+	 */
 	public int countOut() {
 		if (isEmpty())
 			return NEGATIVE;
